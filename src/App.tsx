e191ded122da27dd28ee78acbac7c0a72ad30a38
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-06-23 16:31:44 +0200

    add a failure page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Homepage/Home";
 import Login from "./components/Loginpage/Login";
 import Navbar from "./components/Navbar/Navbar";
+import Fail from "./components/Fail/Fail";
 import { myContext } from "./Context";
 function App() {
   const userObject = useContext(myContext);
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {userObject ? null : <Route path="/login" element={<Login />} />}
+        <Route path="/fail" element={<Fail />} />
       </Routes>
     </>
   );
---
+++ src/components/Fail/Fail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const fail = () => {
+  return <div>failed to authenticate</div>;
+};
+
+export default fail;
